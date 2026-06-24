caa172e003bc9cdc86491d8ea50fb0bc668e7401
tianqianguai <1094410998@qq.com>
2026-06-24 18:46:40 +0800

    chore: build release APK for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,9 +14,18 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getProperty("user.home") + "/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
